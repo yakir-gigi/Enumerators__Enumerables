e93a6f23b421f033a89f5843221f0f01a3df2b69
yakir-gigi <86431039+yakir-gigi@users.noreply.github.com>
2021-09-30 21:23:09 +0300

    Add files via upload

---
+++ Enumerators--Enumerables/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enumerators__Enumerables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int [] array = new int[10];
+            int temp = 0;
+            for (int i = 10; i <=100; i+=10)
+            {
+                
+                 array[temp]= i ;
+                temp++;
+            }
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+            foreach (var item in array)
+            {
+
+            }
+           
+        }
+    }
+}
